6c2bba6dcd3ee0162f73a4a74734e470e528ac7b
Kajal Gupta <kk2347994@gmail.com>
2026-03-19 10:19:56 +0530

    TCS final

--- main50.java
+++ main50.java
@@ -1,6 +1,20 @@
 import java.util.*;
 
 public class main50 {
+  public static int[] twoSum(int arr[],int target){
+     HashMap<Integer,Integer>map = new HashMap<>();
+
+     for(int i=0;i<arr.length;i++){
+      int com = target - arr[i];
+
+      if(map.containsKey(com)){
+        return new int[]{map.get(com),i};
+      }
+        map.put(arr[i],i);
+      
+     }
+     return new int[]{-1,-1};
+  }
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          int n = sc.nextInt();
@@ -9,16 +23,20 @@ public class main50 {
             arr[i] = sc.nextInt();
           }
           int target = sc.nextInt();
+          int ans[] = new int[2];
 
-          for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(arr[i]+arr[j] == target){
-               System.out.println(i + "," + j);
-               return;
-            }
-            }
-          }
-          System.out.println("No Pair Found");
+          // for(int i = 0;i<n;i++){
+          //   for(int j = i+1;j<n;j++){
+          //       if(arr[i]+arr[j] == target){
+          //      System.out.println(i + "," + j);
+          //      return;
+          //   }
+          //   }
+          // }
+          // System.out.println("No Pair Found");
+           ans = twoSum(arr, target);
+          System.out.println(ans[0] + "," + ans[1]);
+          
 
     }
 }
