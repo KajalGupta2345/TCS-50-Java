fe7deef032c94c96e7fa83568dbf732d0601ff98
Kajal Gupta <kk2347994@gmail.com>
2026-03-19 11:46:30 +0530

    TCS Final2

--- main49.java
+++ main49.java
@@ -4,43 +4,40 @@ public class main49 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
           String s = sc.nextLine();
-          String words[] = s.split("\\.");
-          for(int i =0;i<words.length;i++){
-            System.out.println(words[i]);
-          }
-          System.out.println();
-          int i = 0 , j = words.length-1;
-          while(j >= i){
-            String temp = words[i];
-            words[i] = words[j];
-            words[j] = temp;
-            j--;
-            i++;
-          }
-          System.out.println(String.join(" ",words));
+          // String words[] = s.split(" ");
+          // int i = 0 , j = words.length-1;
+          // while(j >= i){
+          //   String temp = words[i];
+          //   words[i] = words[j];
+          //   words[j] = temp;
+          //   j--;
+          //   i++;
+          // }
+          // System.out.println(String.join(" ",words));
 
-    }
-}
+
 // "i.like.this.program.very.much"
 // "i like this program very much"
 
-//          String words[] = s.split("\\.");
-//          ArrayList<String> list = new ArrayList<>();
+         String words[] = s.split("\\.");
+         ArrayList<String> list = new ArrayList<>();
 
-//         // empty strings remove karo
-//         for(String word : words){
-//             if(!word.equals(" ")){
-//                 list.add(word);
-//             }
-//         }
-//           int i = 0, j = list.size() - 1;
+        // empty strings remove karo
+        for(String word : words){
+            if(!word.equals("")){
+                list.add(word);
+            }
+        }
+          int i = 0, j = list.size() - 1;
 
-//         while(i < j){
-//             String temp = list.get(i);
-//             list.set(i, list.get(j));
-//             list.set(j, temp);
-//             i++;
-//             j--;
-//         }
+        while(i < j){
+            String temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+            i++;
+            j--;
+        }
 
-//         return String.join(".", list);
+        System.out.println(String.join(".", list));
+      }
+    }
